1600dd75409e5c4d0e3127fc030dfce03aa9069d
tianNanYiHao <851085835@qq.com>
2018-07-11 20:21:32 +0800

    Initial commit

---
+++ Example/SOCR/LFFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LFFAppDelegate.h
+//  SOCR
+//
+//  Created by tianNanYiHao on 07/11/2018.
+//  Copyright (c) 2018 tianNanYiHao. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LFFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
